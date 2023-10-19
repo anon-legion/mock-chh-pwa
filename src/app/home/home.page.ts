a2b39dd1fb22d22adefbc05c814412f565c1942f
=GV= <g.sync.mobile1@gmail.com>
2023-10-19 17:59:41 +0800

    update onInit function to retrieve token from deployed pn api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,7 +18,28 @@ export class HomePage {
 
   async ngOnInit() {
     console.log('init');
-    const response = await fetch('http://localhost:3000/api/v1/test_endpoint');
-    console.log(response);
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userId: 'testUser2' }),
+    };
+    try {
+      const res = await fetch(
+        // 'http://localhost:3000/api/v1/subscribe',
+        'https://chh-push-notification-production.up.railway.app/api/v1/subscribe',
+        options
+      );
+      const token = await res.json();
+      console.log(token);
+
+      const ws = new WebSocket(token.url);
+
+      ws.onmessage = function (e) {
+        const serverMessage = e.data;
+        console.log(serverMessage);
+      };
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
